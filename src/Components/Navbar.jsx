0e2cb44f8bc0dc204b254674369f633534d758ca
abinesh0309 <abinesh0309@gmail.com>
2025-03-24 16:54:36 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import styles from './Navbar.module.css'
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Tech Events</li>
-          <li>Non-Tech Events</li> 
-          <li>Contact</li>
+      <nav className={styles.navbar}>
+      <img src="/logo/nklogo.png" alt="" srcset="" className={styles.logoimg}/>
+        <ul className={styles.navbarList} >
+          
+          <li><a href="/">HOME</a></li>
+          <li><a href="/tech">TECHNICAL</a></li>
+          <li><a href="/nonetech">NON-TECHNICAL</a></li> 
+          <li><a href="/contact">CONTACT</a></li>
         </ul>
       </nav>
     </div>
