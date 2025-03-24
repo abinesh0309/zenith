4d19606728f62ec39a8d056781d60db96fe73f5f
abinesh0309 <abinesh0309@gmail.com>
2025-03-24 21:02:12 +0530

    homepage finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import styles from './Home.module.css';
 
 const Home = () => {
+  const [animationStarted, setAnimationStarted] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimationStarted(true);
+    }, 100);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      home
+    <div className={styles.container}>
+       <div className={`${styles.imageContainerlogo} ${animationStarted ? styles.animate : ''}`}>
+        <img 
+          src="/logo/worstlogo.png" 
+          alt="logo" 
+          className={styles.image}
+        />
+      </div>
+      
+      <div className={`${styles.imageContainersam} ${animationStarted ? styles.animate : ''}`}>
+        <img 
+          src="/logo/samuraipng.png" 
+          alt="samurai" 
+          className={styles.image}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
